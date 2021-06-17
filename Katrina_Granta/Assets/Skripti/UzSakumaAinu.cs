a3073cf5b1347e0e2798026a56cc6abfda202132
Katrina <katrinagranta30@gmail.com>
2021-06-17 14:08:56 +0300

    Izveidota pareja no testa ainas uz sakuma ainu

---
+++ Katrina_Granta/Assets/Skripti/UzSakumaAinu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UzSakumaAinu : MonoBehaviour
+{
+  public void UzSakumaainu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
