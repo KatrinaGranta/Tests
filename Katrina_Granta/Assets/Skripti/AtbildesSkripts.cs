5ddbd719d1d3cdb062e1204a87ef0055ee06f8e0
Katrina <katrinagranta30@gmail.com>
2021-06-17 13:16:32 +0300

    Spelesparvaldnieka izveidots skripts, kas ļauj programmai ģenerēt jautājumus un savienoties ar atbilžu skriptu. Nosaka cik jautājumi ir atbildēti pareizi. SpelesParvaldienku ievietoju GameObject

--- Katrina_Granta/Assets/Skripti/AtbildesSkripts.cs
+++ Katrina_Granta/Assets/Skripti/AtbildesSkripts.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AtbildesSkripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool AtbildePareiza = false;
+    public SpelesParvaldnieks spelesParvaldnieks;
 
-    // Update is called once per frame
-    void Update()
+    public void Atbilde()
     {
-        
+        if (AtbildePareiza)
+        {
+            Debug.Log("Jautājums atbildēts pareizi!");
+            spelesParvaldnieks.pareizi();
+        }
+        else
+        {
+            Debug.Log("Jautājums atbildēts nepareizi");
+            spelesParvaldnieks.nepareizi();
+        }
     }
 }
--- Katrina_Granta/Assets/Skripti/SpelesParvaldnieks.cs
+++ Katrina_Granta/Assets/Skripti/SpelesParvaldnieks.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpelesParvaldnieks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Jautajumi> jautajumi;
+    public GameObject[] izvele;
+    public int pasreizejaisJautajums;
+
+    public GameObject JautajumuLogs;
+    public GameObject RezultatuLogs;
+
+    public Text jautajumuTeksts;
+    public Text RezultatuTeksts;
+
+    int jautajumuSkaits = 0;
+    public int rezultats;
+
+    private void Start()
     {
-        
+        jautajumuSkaits = jautajumi.Count;
+        RezultatuLogs.SetActive(false);
+        generetJautajumu();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpelesBeigas()
     {
-        
+        JautajumuLogs.SetActive(false);
+        RezultatuLogs.SetActive(true);
+        RezultatuTeksts.text = rezultats + "/" + jautajumuSkaits;
     }
+
+    public void pareizi()
+    {
+        rezultats += 1;
+        jautajumi.RemoveAt(pasreizejaisJautajums);
+        generetJautajumu();
+
+    }
+
+    public void nepareizi()
+    {
+        jautajumi.RemoveAt(pasreizejaisJautajums);
+        generetJautajumu();
+    }
+    void Atbilde()
+    {
+        for(int i = 0; i< izvele.Length; i++)
+        {
+            izvele[i].GetComponent<AtbildesSkripts>().AtbildePareiza = false;
+            izvele[i].transform.GetChild(0).GetComponent<Text>().text = jautajumi[pasreizejaisJautajums].atbildes[i];
+
+            if (jautajumi[pasreizejaisJautajums].PareizaAtbilde == i + 1)
+            {
+                izvele[i].GetComponent<AtbildesSkripts>().AtbildePareiza = true;
+            }
+        }
+    }
+     void generetJautajumu()
+    {
+        if(jautajumi.Count > 0)
+        {
+            pasreizejaisJautajums = Random.Range(0, jautajumi.Count);
+            jautajumuTeksts.text = jautajumi[pasreizejaisJautajums].Jautajums;
+            Atbilde();
+        }
+        else
+        {
+            Debug.Log("Nav vairs jautājumu");
+            SpelesBeigas();
+        }
+    }
+
 }
+
+  
