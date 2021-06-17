5b4345c231ec59f17d74526d98dd3096bbc749c5
Katrina <katrinagranta30@gmail.com>
2021-06-17 12:27:39 +0300

    Izveidota pāreja no sākuma ainas uz testa ainu

---
+++ Katrina_Granta/Assets/Skripti/UzTestaAinu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UzTestaAinu : MonoBehaviour
+{
+    public void UzTestu()
+    {
+        SceneManager.LoadScene("TestaAina", LoadSceneMode.Single);
+    }
+}
